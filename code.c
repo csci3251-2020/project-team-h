7b51823ba10eb3af5a7c03c23b5bea2145c29b10
markm812 <53172013+markm812@users.noreply.github.com>
2020-05-03 22:15:56 +0800

    make it simple

--- code.c
+++ code.c
@@ -1,27 +1,5 @@
-// Your C code here
 #include <stdio.h>
-
-void SayHello () {
-  printf("Hello world.");
-  printf("\n");
-}
-
-void SayBye () {
-  printf("Goodbye world.");
-  printf("\n");
-}
-
-void Counting (int n) {
-  for (int i = 0; i < n; i++) {
-    printf("%d\n",i);
-  }
-  printf("I found you.\n");
-}
-
-void main() {
- SayHello();
- Counting (10);
- SayBye();
-  
- return; 
+int main() {
+   printf("Hello, World!");
+   return 0;
 }
